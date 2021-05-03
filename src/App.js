a38cac6bb8bfb054fc512fba144faaaae0302bc7
suedar <690372513@qq.com>
2021-05-03 18:59:01 +0800

    init

--- src/App.js
+++ src/App.js
@@ -8,38 +8,27 @@ import './App.less';
 const { Content } = Layout;
 
 function App() {
-    return (
-        <Layout className="app" style={{
-            '--color-warning': '#faad14',
-            '--color-success': '#52c41a',
-            '--color-success-bg': '#f6ffed',
-            '--color-error': '#f5222d',
-            '--color-error-bg': '#fff1f0',
-            '--color-info': '#1890ff',
-            }}>
-            <Router>
-                <Content>
-                    <div className="layout-content">
-                        <Switch>
-                            <Route path="/" exact component={Home} />
-                            {/* <Route path="/providers" component={Home} />
-                            <Route path="/jobs" component={Job} exact />
-                            <Route path="/jobs/:id" component={Detail} />
-                            <Route path="/materials" component={Material} />
-                            <Route path="/tools" component={Tool} />
-                            <Route path="/user" component={User} />
-                            <Route path="/login" exact component={Login} />
-                            <Route path="/monitor" exact component={Monitor} />
-                            <Route path="/admin" component={Admin} />
-                            <Route path="/job-log" component={JobLog} />
-                            <Route path="/auto-uploads" component={AutoUpload} />
-                            <Route path="/reports/rounds/:roundId" component={ReportSyncJob} /> */}
-                        </Switch>
-                    </div>
-                </Content>
-            </Router>
-        </Layout>
-    );
+  return (
+    <Layout
+      className="app"
+      style={{
+        '--color-warning': '#faad14',
+        '--color-success': '#52c41a',
+        '--color-success-bg': '#f6ffed',
+        '--color-error': '#f5222d',
+        '--color-error-bg': '#fff1f0',
+        '--color-info': '#1890ff',
+      }}
+    >
+      <Router>
+        <Content>
+          <div className="layout-content">
+            <Home />
+          </div>
+        </Content>
+      </Router>
+    </Layout>
+  );
 }
 
 export default App;
